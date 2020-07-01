0b40d8f5ba446a342b29a5216a22caa52752ddad
Axel <axel.vandenabeele@epitech.eu>
2020-07-01 16:53:48 +0200

    [test] : test picture cloudinary angle

--- app/models/real_estate_picture.rb
+++ app/models/real_estate_picture.rb
@@ -6,7 +6,7 @@ class RealEstatePicture < ApplicationRecord
 		{
 			id: self.id,
 			public_id: self.public_id,
-			url: self.url[0, self.url.index('upload') + "upload".size] + "/a_" + self.angle.to_s + self.url[(self.url.index('upload') + "upload".size)..self.url.size],
+			url: self.url,
 			position: self.position,
 			angle: self.angle,
 			real_estate_id: self.real_estate_id,
