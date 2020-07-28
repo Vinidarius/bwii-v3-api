d5dbd6414fac4f5ace0c7d0210d4135f0492c310
Axel <axel.vandenabeele@epitech.eu>
2020-07-28 10:43:28 +0200

    [feat] : remove token verification for demo

--- app/filters/api/v3/real_estate_types_filter.rb
+++ app/filters/api/v3/real_estate_types_filter.rb
@@ -3,9 +3,9 @@ class Api::V3::RealEstateTypesFilter < Api::V3::BaseFilter
   def collection
     real_estate_types = self.resource
 
-		unless params[:compagny_id].blank?
-			real_estate_types = real_estate_types.where('real_estate_types.compagny_id = ?', params[:compagny_id])
-		end
+		# unless params[:compagny_id].blank?
+		# 	real_estate_types = real_estate_types.where('real_estate_types.compagny_id = ?', params[:compagny_id])
+		# end
 
    return self.with_associations(real_estate_types)
   end
--- app/filters/api/v3/user_types_filter.rb
+++ app/filters/api/v3/user_types_filter.rb
@@ -2,13 +2,13 @@ class Api::V3::UserTypesFilter < Api::V3::BaseFilter
 
   def collection
     @user_types = self.resource
-		Agent.all.pluck(:compagny_id, :tokens).each do |el|
-			if params[:token].eql? el[1].keys.first
-				@compagny = el[0]
-			end
-		end
-
-		@compagny ? (@user_types = @user_types.where(compagny_id: @compagny)) : (return [])
+		# Agent.all.pluck(:compagny_id, :tokens).each do |el|
+		# 	if params[:token].eql? el[1].keys.first
+		# 		@compagny = el[0]
+		# 	end
+		# end
+		#
+		# @compagny ? (@user_types = @user_types.where(compagny_id: @compagny)) : (return [])
 
    return self.with_associations(@user_types)
   end
