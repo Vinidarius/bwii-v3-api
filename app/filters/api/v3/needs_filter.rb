b7f17f85a2d726df8564ec602a9c0e5d056b0785
Axel <axel.vandenabeele@epitech.eu>
2020-09-02 15:56:19 +0200

    [feat] : add pagination for needs

--- app/filters/api/v3/needs_filter.rb
+++ app/filters/api/v3/needs_filter.rb
@@ -7,6 +7,10 @@ class Api::V3::NeedsFilter < Api::V3::BaseFilter
 			@needs = @needs.joins(:need_links).where(need_links: {user_id: params[:user_id]})
 		end
 
+		if params[:page] && params[:nbr]
+			return self.with_associations(@needs.page(params[:page]).per(params[:nbr]))
+		end
+
    return self.with_associations(@needs)
   end
 
