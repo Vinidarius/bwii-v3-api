fa330feecbdcdf80b00a3e31f9835d8c123610d4
Axel <axel.vandenabeele@epitech.eu>
2020-09-07 10:08:58 +0200

    [fix] : need_links filter

--- app/filters/api/v3/need_links_filter.rb
+++ app/filters/api/v3/need_links_filter.rb
@@ -12,7 +12,7 @@ class Api::V3::NeedLinksFilter < Api::V3::BaseFilter
 		end
 
 		unless params[:user_id].blank?
-			@need_links = @need_links.includes(:need).where(need: {user_id: params[:user_id]})
+			@need_links = @need_links.where('need_links.user_id = ?', params[:user_id])
 		end
 
    return self.with_associations(@need_links)
