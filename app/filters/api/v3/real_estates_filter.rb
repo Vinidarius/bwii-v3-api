c5b271dbac80a06199447fb54aab5d2dec0fbaa7
Axel <axel.vandenabeele@epitech.eu>
2020-12-11 16:13:32 +0100

    [test] : test sector name filter

--- app/filters/api/v3/real_estates_filter.rb
+++ app/filters/api/v3/real_estates_filter.rb
@@ -20,7 +20,7 @@ class Api::V3::RealEstatesFilter < Api::V3::BaseFilter
 			@valid_real_estates = []
 
 			# @valid_real_estates = @real_estates.where("title ILIKE '%#{params[:title]}%'").pluck(:id)
-			@valid_real_estates = @real_estates.joins(:sectors).where(sectors: {name: "ILIKE '%#{params[:title]}%'"}).pluck(:id)
+			@valid_real_estates = @real_estates.joins(:sectors).where("sectors.name ILIKE ?", "%#{params[:title]}%").pluck(:id)
 
 			@real_estates = @real_estates.where(id: @valid_real_estates);
 		end
