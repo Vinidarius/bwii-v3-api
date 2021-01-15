35989a8eee966b076872224154ba88a2ffa2bf93
Axel <axel.vandenabeele@epitech.eu>
2021-01-15 16:24:47 +0100

    [feat] : add user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,12 @@
 class UserMailer < ApplicationMailer
+
+	def reset_password(user)
+		mail(
+			from: "support@b-wii.fr",
+			to: user.email,
+			subject: "Changement de mot de passe BWII",
+			delivery_method_options: { api_key: '59795337f073950857377d3604a60493', secret_key: '8e5f3fd672ea238347aaa6582e1ea7d2', version: "v3.1" }
+		)
+	end
+
 end
