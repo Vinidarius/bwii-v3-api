3e0061009403c576446e0522adf6c81145964dd8
Axel <axel.vandenabeele@epitech.eu>
2021-03-24 19:01:49 +0100

    [feat] : init all password

--- app/jobs/reset_password_job.rb
+++ app/jobs/reset_password_job.rb
@@ -4,10 +4,10 @@ class ResetPasswordJob < ApplicationJob
   def perform(*args)
     User.all.each do |user|
 			puts user.id
-			user.reset_password_token = 'temp'
-			@password = user.lastname[0].upcase + user.lastname[0].upcase + "-2020-Bwii";
-			user.reset_password(@password, @password);
-			user.save
+			# user.reset_password_token = 'temp'
+			# @password = user.lastname[0].upcase + user.lastname[0].upcase + "-2020-Bwii";
+			# user.reset_password(@password, @password);
+			# user.save
 		end
   end
 end
