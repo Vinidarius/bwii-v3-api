02b035a2cdd1c96d273cf9f8a4099052aaef49cf
Axel <axel.vandenabeele@epitech.eu>
2021-04-20 22:09:34 +0200

    [feat] : note_links render

--- app/models/note_link.rb
+++ app/models/note_link.rb
@@ -7,9 +7,9 @@ class NoteLink < ApplicationRecord
 	def render_api
 		{
 			id: self.id,
-			note_id: self.note_id,
-			real_estate_id: self.real_estate_id ? RealEstate.find_by(id: self.real_estate_id).render_list_api : nil,
-			user_id: self.user_id ? User.find_by(id: self.user_id).render_list_api : nil,
+			note_id: Note.find_by(id: self.note_id).render_api,
+			real_estate_id: self.real_estate_id,
+			user_id: self.user_id,
 		}
 	end
 
@@ -17,8 +17,8 @@ class NoteLink < ApplicationRecord
 		{
 			id: self.id,
 			note_id: self.note_id,
-			real_estate_id: self.real_estate_id,
-			user_id: self.user_id
+			real_estate_id: self.real_estate_id ? RealEstate.find_by(id: self.real_estate_id).render_list_api : nil,
+			user_id: self.user_id ? User.find_by(id: self.user_id).render_list_api : nil
 		}
 	end
 
