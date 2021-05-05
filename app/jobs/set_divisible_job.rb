60a8e782121de428efec04fb5cbf8233fd21262d
Axel <axel.vandenabeele@epitech.eu>
2021-05-06 00:37:52 +0200

    [feat] : set divisible job v1

---
+++ app/jobs/set_divisible_job.rb
@@ -0,0 +1,16 @@
+class SetDivisibleJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+		RealEstate.all.each do |real_estate|
+			@min_val = 0;
+			real_estate.buildings.each do |building|
+				puts building.divisible
+				if building.divisible && building.divisible_to_i != 0 && building.divisible.to_i < @min_value
+					@min_val = building.divisible_to_i
+				end
+			end
+			puts @min_val
+    end
+  end
+end
---
+++ test/jobs/set_divisible_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SetDivisibleJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
