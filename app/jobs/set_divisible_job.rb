55efc00547df7bb6bc843c709156df85bd69f1f6
Axel <axel.vandenabeele@epitech.eu>
2021-05-06 06:31:53 +0200

    [feat] : set divisible job v1

--- app/jobs/set_divisible_job.rb
+++ app/jobs/set_divisible_job.rb
@@ -5,12 +5,14 @@ class SetDivisibleJob < ApplicationJob
 		RealEstate.all.each do |real_estate|
 			@min_val = 0;
 			real_estate.buildings.each do |building|
-				puts building.divisible
 				if building.divisible && building.divisible.to_i != 0 && (@min_val == 0 || building.divisible.to_i < @min_val)
 					@min_val = building.divisible.to_i
 				end
 			end
-			puts @min_val
+			if @min_val > 0
+				real_estate.divisible = @min_val
+				real_estate.save
+			end
     end
   end
 end
