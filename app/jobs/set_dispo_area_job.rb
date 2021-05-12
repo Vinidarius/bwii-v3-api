4d4b7deef08a172a8bbcf8472823775ef5507697
Axel <axel.vandenabeele@epitech.eu>
2021-05-12 15:59:15 +0200

    [fix] : dispo area

--- app/jobs/set_dispo_area_job.rb
+++ app/jobs/set_dispo_area_job.rb
@@ -2,13 +2,16 @@ class SetDispoAreaJob < ApplicationJob
   queue_as :default
 
 	def perform(*args)
-		RealEstate.limit(300).each do |real_estate|
+		RealEstate.limit(10).each do |real_estate|
 			@dispo_area = 0;
 			real_estate.buildings.each do |building|
 				building.floors.each do |floor|
 					floor.rooms.each do |room|
+						puts room.fill
+						puts room.area
 						if !room.fill
 							@dispo_area += room.area.to_i
+							puts @dispo_area
 						end
 					end
 				end
